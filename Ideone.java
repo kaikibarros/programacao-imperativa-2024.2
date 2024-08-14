5251f7d26607756adfb6fb84a0566e09df94ed9c
kaikibarros <kaikibarros2004@gmail.com>
2024-08-14 17:30:03 -0300

    biblioteca-nao-responde

--- Ideone.java
+++ Ideone.java
@@ -1,16 +1,18 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Ideone
 {
     public static void main (String[] args) throws java.lang.Exception
     {
-        printNumbers1To100();
-        printSumOfOddNumbers();
-        printMultiplesOf3NotEndingIn3();
+        number1to100();
+        somatodosimpares();
+        multiplosde3sem3();
     }
 
     // Função para imprimir os números de 1 a 100 separados por vírgula
-    public static void printNumbers1To100() {
+    public static void number1to100() {
         for (int i = 1; i <= 100; i++) {
             if (i > 1) {
                 System.out.print(", ");
@@ -21,7 +23,7 @@ class Ideone
     }
 
     // Função para imprimir o somatório de todos os números ímpares entre 10 e 1000
-    public static void printSumOfOddNumbers() {
+    public static void somatodosimpares() {
         int sum = 0;
         for (int i = 11; i < 1000; i += 2) {
             sum += i;
@@ -30,7 +32,7 @@ class Ideone
     }
 
     // Função para imprimir todos os números múltiplos de 3 que não terminem em 3
-    public static void printMultiplesOf3NotEndingIn3() {
+    public static void multiplosde3sem3() {
         boolean first = true;
         for (int i = 3; i < 1000; i += 3) {
             if (i % 10 != 3) {
