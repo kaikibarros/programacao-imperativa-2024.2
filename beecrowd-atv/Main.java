7d8322d74988ae94f63237d64e008665c74341f3
kaikibarros2004@gmail.com <kaikibarros2004@gmail.com>
2024-10-11 01:33:42 -0300

    atualizando

--- beecrowd-atv/Main.java
+++ beecrowd-atv/Main.java
@@ -1,32 +1,18 @@
 import java.io.IOException;
 import java.util.Scanner; 
-
-
-
-
 public class Main {
-
     public static void main(String[] args)  { 
         Scanner ler = new Scanner(System.in);
-
-        int[] N = new int[10];
-        
-        System.out.print("iNSIRA UM NÚMEIRO: ")
-        N[0] = ler.nextInt();
-
-        System.out.println("Digite um número inteiro: ");
-        for(int i = 0; i < N.length; i++){
-            N[i] = N[i -1] * 2;
+        int[]N= new int[10];
+        N[0]=ler.nextInt();
+        for(int i=1; i< N.length;i++){
+            N[i] = N[i - 1]*2;
         }
-       
-        System.out.println("Resultante:");
-        // N = ler.nextInt(); 
-        for(int i = 0; i < N.length; i++){
-            System.outprintln("N[" , i , "] =" , N[i]);
+        for(int i=0;i< N.length; i++){
+            System.out.println("N["+i+"] ="+N[i]);
         }
         ler.close();
-
-
     }
- 
 }
+
+// FUNCIONOU
---
+++ beecrowd-atv/array-fill2/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner ler = new Scanner(System.in);
+        int T = ler.nextInt();
+        int V = 0;
+        int[] N = new int[10];
+        for (int i = 0; i < 1000; i++) {
+            N[i] = i % T; 
+        }
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("N[" + i + "] = " + N[i]);
+        }
+        ler.close();
+    }
+}
