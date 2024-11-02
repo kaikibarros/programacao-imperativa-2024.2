efcc3913c7913a76446366f2765b096526d58bb1
Kaiki Barros <104598739+kaikibarros@users.noreply.github.com>
2024-11-02 19:52:57 +0000

    v07 e v08 feitos

--- listaVetores/v07.java
+++ listaVetores/v07.java
@@ -1,1 +1,31 @@
-import 
+package listaVetores;
+import java.util.Scanner;
+
+// Escreva um programa que leia 10 numeros inteiros e os armazene em um vetor. Imprima ´
+//o vetor, o maior elemento e a posic¸ao que ele se encontra.
+
+public class v07 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int[] vetor = new int[10];
+
+        for(int i = 0; i < vetor.length; i++){
+            System.out.printf("digite o %d número: ", i + 1);
+            vetor[i] = ler.nextInt();
+        }
+        int maiorElemento = vetor[0];
+        int posicaoMaior = 0;
+
+        for(int i = 1; i < vetor.length; i++){
+            if(vetor[i] > maiorElemento) {
+                 maiorElemento = vetor[i] ;
+                 posicaoMaior = i;
+            }
+        }
+        System.out.printf(" o maior elemento é %d%n",maiorElemento);
+        System.out.printf("e a sua posição é a %d",posicaoMaior + 1);
+
+        ler.close();
+    }
+}
---
+++ listaVetores/v08.java
@@ -0,0 +1,20 @@
+package listaVetores;
+import java.util.Scanner;
+//Crie um programa que le 6 valores inteiros e, em seguida, mostre na tela os valores lidos ˆ
+//na ordem inversa.
+
+public class v08 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int[] vetor = new int[6];
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.printf("Digite o %d valor: ", i + 1);
+            vetor[i] = ler.nextInt();
+        }
+       for (int i = vetor.length - 1; i >= 0; i--) {
+           System.out.println(vetor[i]);
+       }
+    }
+}
